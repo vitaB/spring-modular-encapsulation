b6d53889079b9911d3732daf9eec12211d48bf32
Viktor Befort <v.befort@googlemail.com>
2021-02-11 21:16:21 +0100

    use JpaProperties Configuration class, instead of reading envs

--- ModuleA/src/main/kotlin/com/example/moduleA/ConfigA.kt
+++ ModuleA/src/main/kotlin/com/example/moduleA/ConfigA.kt
@@ -2,13 +2,13 @@ package com.example.moduleA
 
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
-import org.springframework.core.env.Environment
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
@@ -26,27 +26,29 @@ import javax.sql.DataSource
         entityManagerFactoryRef = "moduleAEntityManager",
         transactionManagerRef = "moduleATransactionManager"
 )
-class ConfigurationModuleA internal constructor(val env: Environment) {
+class ConfigurationModuleA {
 
     @Bean(name = ["moduleADataSource"])
     @ConfigurationProperties(prefix = "module-a.spring.datasource")
     fun moduleADataSource(): DataSource = DataSourceBuilder.create().build()
 
     @Bean(name = ["moduleAEntityManager"])
-    //@ConfigurationProperties(prefix = "module-a.spring.jpa")
-    fun moduleAEntityManager(@Qualifier("moduleADataSource")
+    fun moduleAEntityManager(@Qualifier("moduleAJPAProperties")
+                             properties: JpaProperties,
+                             @Qualifier("moduleADataSource")
                              dataSource: DataSource): LocalContainerEntityManagerFactoryBean {
         val em = LocalContainerEntityManagerFactoryBean()
         em.dataSource = dataSource
         em.setPackagesToScan("com.example.moduleA")
         val vendorAdapter = HibernateJpaVendorAdapter()
         em.jpaVendorAdapter = vendorAdapter
-        val properties = hashMapOf(
-                "hibernate.dialect" to env.getProperty("module-a.spring.jpa.properties.hibernate.dialect"),
-                "hibernate.default_schema" to env.getProperty("module-a.spring.jpa.properties.hibernate.default_schema"))
-        em.setJpaPropertyMap(properties)
+        em.setJpaPropertyMap(properties.properties)
         return em
     }
+
+    @Bean(name = ["moduleAJPAProperties"])
+    @ConfigurationProperties(prefix = "module-a.spring.jpa")
+    fun moduleAJPAProperties(): JpaProperties = JpaProperties()
 
     @Bean(name = ["moduleATransactionManager"])
     fun moduleATransactionManager(@Qualifier("moduleAEntityManager")
--- ModuleB/src/main/kotlin/com/example/moduleB/ConfigB.kt
+++ ModuleB/src/main/kotlin/com/example/moduleB/ConfigB.kt
@@ -2,13 +2,13 @@ package com.example.moduleB
 
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
-import org.springframework.core.env.Environment
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
@@ -26,27 +26,29 @@ import javax.sql.DataSource
         entityManagerFactoryRef = "moduleBEntityManager",
         transactionManagerRef = "moduleBTransactionManager"
 )
-class ConfigurationModuleB internal constructor(val env: Environment) {
+class ConfigurationModuleB {
 
     @Bean(name = ["moduleBDataSource"])
     @ConfigurationProperties(prefix = "module-b.spring.datasource")
     fun moduleBDataSource(): DataSource = DataSourceBuilder.create().build()
 
     @Bean(name = ["moduleBEntityManager"])
-    //@ConfigurationProperties(prefix = "module-b.spring.jpa")
-    fun moduleBEntityManager(@Qualifier("moduleBDataSource")
+    fun moduleBEntityManager(@Qualifier("moduleBJPAProperties")
+                             properties: JpaProperties,
+                             @Qualifier("moduleBDataSource")
                              dataSource: DataSource): LocalContainerEntityManagerFactoryBean {
         val em = LocalContainerEntityManagerFactoryBean()
         em.dataSource = dataSource
         em.setPackagesToScan("com.example.moduleB")
         val vendorAdapter = HibernateJpaVendorAdapter()
         em.jpaVendorAdapter = vendorAdapter
-        val properties = hashMapOf(
-                "hibernate.dialect" to env.getProperty("module-b.spring.jpa.properties.hibernate.dialect"),
-                "hibernate.default_schema" to env.getProperty("module-b.spring.jpa.properties.hibernate.default_schema"))
-        em.setJpaPropertyMap(properties)
+        em.setJpaPropertyMap(properties.properties)
         return em
     }
+
+    @Bean(name = ["moduleBJPAProperties"])
+    @ConfigurationProperties(prefix = "module-b.spring.jpa")
+    fun moduleBJPAProperties(): JpaProperties = JpaProperties()
 
     @Bean(name = ["moduleBTransactionManager"])
     fun moduleBTransactionManager(@Qualifier("moduleBEntityManager")
